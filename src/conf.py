0ef53ea4b40847261556ed4e51d0e21835155507
Antonio Cuni <anto.cuni@gmail.com>
2021-03-23 16:39:53 +0100

    customize with HPy-specific titles

--- src/conf.py
+++ src/conf.py
@@ -16,16 +16,16 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Your Name"  # (translatable)
-BLOG_TITLE = "Demo Site"  # (translatable)
+BLOG_AUTHOR = "The HPy Team"  # (translatable)
+BLOG_TITLE = "HPy"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://hpyproject.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
-BLOG_EMAIL = "joe@demo.site"
-BLOG_DESCRIPTION = "This is a demo site for Nikola."  # (translatable)
+BLOG_EMAIL = "hpy-dev@python.ord"
+BLOG_DESCRIPTION = "A better C API for Python"  # (translatable)
 
 # Nikola is multilingual!
 #
