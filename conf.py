c488596f9d79f29266f6b943e3e2101ed1b46694
Antonio Cuni <anto.cuni@gmail.com>
2021-03-24 11:38:09 +0100

    copy&paste the very same theme used by pypy.org

--- conf.py
+++ conf.py
@@ -102,7 +102,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog4"
+THEME = "hpy"
 
 # A theme color. In default themes, it might be displayed by some browsers as
 # the browser UI color (eg. Chrome on Android). Other themes might also use it
