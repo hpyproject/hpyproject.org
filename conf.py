154d7f164df49b7dd7b1aecbee7eb0dfc61dcb9e
Antonio Cuni <anto.cuni@gmail.com>
2021-03-29 18:20:41 +0200

    try to fix utteranc.es

--- conf.py
+++ conf.py
@@ -989,7 +989,7 @@ COMMENT_SYSTEM = 'utterances'
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "hpyproject/hpyproject.github.io"
+COMMENT_SYSTEM_ID = "hpyproject/hpyproject.org"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
