3a1ccbe07ec3fe7e1096568baba6c7e1a7ae5797
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-05-05 11:06:13 -0400

    Add psf/black

--- listings/2021/03/hello-hpy/setup.py
+++ listings/2021/03/hello-hpy/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, Extension
+
 setup(
     name="hello",
-    ext_modules = [
-        Extension('hello_old', ['hello_old.c']),
+    ext_modules=[
+        Extension("hello_old", ["hello_old.c"]),
     ],
-    hpy_ext_modules = [
-        Extension('hello_new', ['hello_new.c']),
+    hpy_ext_modules=[
+        Extension("hello_new", ["hello_new.c"]),
     ],
-    setup_requires=['hpy.devel'],
+    setup_requires=["hpy.devel"],
 )
